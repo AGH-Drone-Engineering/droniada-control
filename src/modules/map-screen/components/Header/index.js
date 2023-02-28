1510eaa0b88536518249da7a2df38913ac0e24fb
atomwoz <atwozniak67@gmail.com>
2023-02-28 22:04:22 +0100

    Added routes and dynamicly map center

--- src/modules/map-screen/components/Header/index.js
+++ src/modules/map-screen/components/Header/index.js
@@ -1,10 +1,20 @@
 export default function Header() {
   return (
     <header>
-    <h1>
-      Droniada 2023
-    </h1>
-    <img src={process.env.PUBLIC_URL + '/agh-de-logo.png'} alt='agh-de-logo' />
-  </header>
+      <div className="links">
+        <a href="/droniada-control/intruder">Intruz</a>
+        <a href="/droniada-control/pipeline">Rurociąg</a>
+        <a href="/droniada-control/tree">Drzewo życia</a>
+      </div>
+      <div className="head1">
+        <h1>
+          Droniada 2023
+        </h1>
+        <img src={process.env.PUBLIC_URL + '/agh-de-logo.png'} alt='agh-de-logo' />
+      </div>
+      <div className="empty">
+
+      </div>
+    </header>
   );
 }
