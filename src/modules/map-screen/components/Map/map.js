93ec8a580ce9708781ae75836c1b577400fa4369
Carbon225 <carbon225@protonmail.com>
2023-03-01 22:28:54 +0100

    fix eslint

--- src/modules/map-screen/components/Map/map.js
+++ src/modules/map-screen/components/Map/map.js
@@ -8,15 +8,14 @@ function MapMover(p) {
   return null;
 }
 
-export default function MapRenderer(props) {
-  const position = props.position;
+export default function MapRenderer({ position, db }) {
   return (
     <MapContainer center={position} zoom={17} style={{ height: '100%' }}>
       <TileLayer
         url='https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.jpg?access_token=pk.eyJ1IjoiYWdoLWRlIiwiYSI6ImNsZWxvajl3dDBtd3Qzd29kcnY4YjFxbmMifQ.xUC7tSyVs0LcHrdAf3XNgA'
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
       />
-      <Points db={props.db}></Points>
+      <Points db={db}></Points>
       <MapMover position={position}></MapMover>
     </MapContainer>
   );
