741872194a155642d8b1fa240197ab840c1c81ec
Carbon225 <carbon225@protonmail.com>
2023-03-01 22:19:29 +0100

    router links

--- src/modules/map-screen/components/Header/index.js
+++ src/modules/map-screen/components/Header/index.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
       <div className="links">
-        <a href="/droniada-control/intruder">Intruz</a>
-        <a href="/droniada-control/pipeline">Rurociąg</a>
-        <a href="/droniada-control/tree">Drzewo życia</a>
+        <Link to="/intruder">Intruz</Link>
+        <Link to="/pipeline">Rurociąg</Link>
+        <Link to="/tree">Drzewo życia</Link>
       </div>
       <div className="head1">
         <h1>
