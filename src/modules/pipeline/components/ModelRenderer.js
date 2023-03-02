267d045eb58784b95bd5b2194690c8fc5d0f88a2
atomwoz <atwozniak67@gmail.com>
2023-03-02 18:46:26 +0100

    Added 3d model renderer from database

--- src/modules/pipeline/components/ModelRenderer.js
+++ src/modules/pipeline/components/ModelRenderer.js
@@ -1,26 +1,38 @@
 /* eslint-disable react/no-unknown-property */
 import React, { useEffect, useRef, useState } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { OrbitControls } from '@react-three/drei';
+import { ref, getDownloadURL } from 'firebase/storage';
+import { storage } from 'logic/fb';
 
 export default function ModelRenderer() {
   const [gltf, setGltf] = useState([]);
   const meshRef = useRef();
 
   useEffect(() => {
-    const loader = new GLTFLoader();
-    loader.load(process.env.PUBLIC_URL + '/model.glb', (gltf) => {
-      setGltf(gltf);
-    });
+    getDownloadURL(ref(storage, '/untitled.obj'))
+      .then((url) => {
+        const loader = new OBJLoader();
+        loader.load(url, (gltf) => {
+          setGltf(gltf);
+        });
+      })
+      .catch((error) => {
+        setGltf({ gltf_error_fb: error });
+      });
   }, []);
 
   if (gltf.length === 0) {
     return <h2>Ładowanie modelu 3D...</h2>;
   }
 
+  if ('gltf_error_fb' in gltf) {
+    return <h2 style={{ color: 'red' }}>Wystąpił błąd podczas ładowania modelu, spróbuj odświeżyc aplikację.</h2>;
+  }
+
   return (
-    <Canvas camera={{ position: [12, 10, 12.25], fov: 30 }}
+    <Canvas camera={{ position: [20, 20, 20], fov: 60 }}
       style={{
         backgroundColor: '#111a21',
         width: '100vw',
@@ -29,12 +41,12 @@ export default function ModelRenderer() {
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <mesh ref={meshRef} scale={[10, 10, 10]}>
-        <primitive object={gltf.scene} />
+      <mesh ref={meshRef} scale={[5, 5, 5]}>
+        <primitive object={gltf} />
         <meshStandardMaterial color={'#AABBCC'} metalness={0.2} />
       </mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <OrbitControls/>
+
+      <OrbitControls />
     </Canvas>
   );
 }
