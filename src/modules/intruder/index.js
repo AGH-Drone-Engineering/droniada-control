95bb7a80811c5cc7b46b7d506a3d4665f0504975
atomwoz <atwozniak67@gmail.com>
2023-03-04 17:44:04 +0100

    Added filtering on right panel

--- src/modules/intruder/index.js
+++ src/modules/intruder/index.js
@@ -2,25 +2,28 @@ import Header from 'components/Header';
 import UnorderedPoints from 'components/UnorderedPoints';
 import MapRenderer from 'components/Map';
 import useInitalLocation from 'logic/useInitalLocation';
+import React, { useState } from 'react';
+import { FilterContext } from 'components/FilterContext';
 
 export default function IntruderScreen() {
   const [position] = useInitalLocation();
+  const [filter, setFilter] = useState({});
+
   return (
     <div className='App'>
-
       <Header appName='Intruz' />
       <hr></hr>
 
-      <main>
-        <div className='map-wrapper'>
-          <MapRenderer position={position} db={'map-points'}></MapRenderer>
-        </div>
-        <div className='right-list'>
-            <h2><u>Znalezione punkty</u></h2>
-            <UnorderedPoints db={'map-points'}></UnorderedPoints>
-        </div>
-      </main>
-
+      <FilterContext.Provider value={{ filter, setFilter }}>
+        <main>
+          <div className='map-wrapper'>
+            <MapRenderer position={position} db={'map-points'} />
+          </div>
+          <div className='right-list'>
+            <UnorderedPoints db={'map-points'} />
+          </div>
+        </main>
+      </FilterContext.Provider>
     </div>
   );
 }
