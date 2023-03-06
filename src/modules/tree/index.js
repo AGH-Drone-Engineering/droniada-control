ef0000375433e3f0f9bcaa457e974e78a0d9b470
atomwoz <atwozniak67@gmail.com>
2023-03-06 22:10:57 +0100

    Added debuging features for huge amount of points

--- src/modules/tree/index.js
+++ src/modules/tree/index.js
@@ -1,17 +1,33 @@
-
 import Header from 'components/Header';
+import UnorderedPoints from 'components/UnorderedPoints';
+import MapRenderer from 'components/Map';
+import useInitalLocation from 'logic/useInitalLocation';
+import React, { useState } from 'react';
+import { FilterContext } from 'logic/FilterContext';
+import useLotsOfPoints from 'debug/lots-of-points';
 
-export default function TreeScreen() {
+const screenDatabase = 'lots-of-points';
+
+export default function IntruderScreen() {
+  const [position] = useInitalLocation(screenDatabase);
+  const [filter, setFilter] = useState({});
+  useLotsOfPoints();
+
   return (
     <div className='App'>
-
-      <Header appName='Drzewo Życia' />
+      <Header appName='Drzewo życia' />
       <hr></hr>
 
-      <main>
-       <h1>Drzewo życia</h1>
-      </main>
-
+      <FilterContext.Provider value={{ filter, setFilter }}>
+        <main>
+          <div className='map-wrapper'>
+            <MapRenderer position={position} db={screenDatabase} />
+          </div>
+          <div className='right-list'>
+            <UnorderedPoints db={screenDatabase} />
+          </div>
+        </main>
+      </FilterContext.Provider>
     </div>
   );
 }
