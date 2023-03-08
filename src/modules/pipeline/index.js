344a8af9e00d3c1f4a8f167fa889deb39d5e58c1
atomwoz <atwozniak67@gmail.com>
2023-03-08 18:19:24 +0100

    Added photo map and map to pipeline
    With tabs  ;)

--- src/modules/pipeline/index.js
+++ src/modules/pipeline/index.js
@@ -1,19 +1,56 @@
 
 import Header from 'components/Header';
 import ModelRenderer from './components/ModelRenderer';
+import { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import LikeSatelliteMap from './components/LikeSatelliteMap';
+import UnorderedPoints from 'components/UnorderedPoints';
+import MapRenderer from 'components/Map';
+import useInitalLocation from 'logic/useInitalLocation';
+import { FilterContext } from 'logic/FilterContext';
 
 export default function PipelineScreen() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (index) => {
+    setTabIndex(index);
+  };
+  const screenDatabase = 'pipeline-map';
+  const [position] = useInitalLocation(screenDatabase);
+  const [filter, setFilter] = useState({});
+
   return (
     <div className='App'>
 
       <Header appName='Rurociąg' />
       <hr></hr>
+      <Tabs selectedIndex={tabIndex} onSelect={handleTabChange}>
+        <TabList>
+          <Tab>Model 3D skanu terenu</Tab>
+          <Tab>Obraz mapy z lotu ptaka</Tab>
+          <Tab>Mapa z zaznaczonymi punktami</Tab>
+        </TabList>
 
-      <main>
-        <h2>Rurociąg</h2>
-      </main>
-
-      <ModelRenderer></ModelRenderer>
+        <TabPanel>
+          <nav className='renderer-menu'></nav>
+          <ModelRenderer />
+        </TabPanel>
+        <TabPanel>
+          <LikeSatelliteMap />
+        </TabPanel>
+        <TabPanel>
+          <FilterContext.Provider value={{ filter, setFilter }}>
+            <main>
+              <div className='map-wrapper'>
+                <MapRenderer position={position} db={screenDatabase} />
+              </div>
+              <div className='right-list'>
+                <UnorderedPoints db={screenDatabase} />
+              </div>
+            </main>
+          </FilterContext.Provider>
+        </TabPanel>
+      </Tabs>
 
     </div>
   );
