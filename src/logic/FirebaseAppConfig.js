99e2461e39e21199a695d302c3e72ed97233173d
atomwoz <atwozniak67@gmail.com>
2023-03-26 21:44:45 +0200

    Added system demo

--- src/logic/FirebaseAppConfig.js
+++ src/logic/FirebaseAppConfig.js
@@ -3,7 +3,7 @@ import { onSnapshot, collection } from 'firebase/firestore';
 import { db } from 'logic/fb';
 
 export default function useAppConfig() {
-  const [config, setConfig] = useState({ stream_source: '' });
+  const [config, setConfig] = useState({ stream_source: '', demo_video: '' });
 
   useEffect(() => {
     return onSnapshot(collection(db, 'configuration'), (querySnapshot) => {
