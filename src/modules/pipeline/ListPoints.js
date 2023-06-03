779eadb0679ddd751c645c624572cd8d713e69c8
atomwoz <84351511+atomwoz@users.noreply.github.com>
2023-06-03 04:56:29 +0200

    Update ListPoints.js

--- src/modules/pipeline/ListPoints.js
+++ src/modules/pipeline/ListPoints.js
@@ -4,6 +4,6 @@ export default function ListPoints() {
   const points = useMapPoints('intruder-points');
 
   return <><h1>{points.map((x) => (
-    <div>`Timestamp: ${x.timestamp}, Point: ${x.name}`</div>
+    <div>`Timestamp: ${x.timestamp.toDate().toLocaleTimeString('pl-PL')}, Point: ${x.name}`</div>
   ))}</h1><br/><br/></>;
 }
